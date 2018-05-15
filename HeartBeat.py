9980cda4c094469b0fd6ac06e1ece7344bdda7d1
Xhesi Galanxhi <xhesi.galanxhi@drei.com>
2018-05-15 17:20:55 +0200

    First working copy of heartbeat with multithreading

--- HeartBeat.py
+++ HeartBeat.py
@@ -1,39 +1,91 @@
 import socket
 import threading
+import time
+
 
 class HeartBeat():
 
-    def __init__(self, ip='127.0.0.1', broadcast='127.0.0.1', port=50007):
+    def __init__(self, name, ip='127.0.0.1', broadcast='127.0.0.1', port=50007, heartbeat_interval=10, ttl=10):
         threading.Thread.__init__(self)
-        print('__init__ called')
+        # Networking parameters
         self.ip = ip
         self.port = port
         self.broadcast = broadcast
-        print(self.ip)
+
+
+        # Socket initialisation
         self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.serversocket.bind((self.broadcast, self.port))
         self.clientsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.receiveThread = None
+        self.sendThread = None
         self.running = True
+
+        # Other parameters
+        self.heartbeat_interval = heartbeat_interval
+        self.name = name
+        self.ttl = ttl
+        # Lists
+        self.nodes = {self.name: [True, 0]}
 
     def stop(self):
         self.running = False
-        print('stop called')
-        self.receiveThread.join()
+        self.clientsocket.shutdown(socket.SHUT_WR)
+        self.serversocket.shutdown(socket.SHUT_WR)
+        print('Exiting Program...')
         self.serversocket.close()
         self.clientsocket.close()
 
-
     def send(self):
-        message = "Hello, World!"
-        self.clientsocket.sendto(message.encode(), (self.broadcast, self.port))
-        self.clientsocket.shutdown(socket.SHUT_WR)
+        while self.running:
+            try:
+                message = self.name
+                self.clientsocket.sendto(message.encode(), (self.broadcast, self.port))
+                time.sleep(self.heartbeat_interval)
+                # TODO: Move this code somewhere else
+                self.increment_ttl()
+                print(self.nodes)
+            except:
+                pass
 
-    def startReceiving(self):
+    def start_receiving(self):
         self.receiveThread = threading.Thread(target=self.receive)
         self.receiveThread.start()
 
+    def start_sending(self):
+        self.sendThread = threading.Thread(target=self.send)
+        self.sendThread.start()
+
     def receive(self):
         while self.running:
-            data, addr = self.serversocket.recvfrom(1024)  # buffer size is 1024 bytes
-            print("received message:", data, " from : ", addr)
+            try:
+                data, addr = self.serversocket.recvfrom(1024)  # buffer size is 1024 bytes
+                #print("received message:", data, " from : ", addr[0])
+                threading.Thread(target=self.parse_data, args=(data.decode("utf-8"),)).start()
+            except:
+                pass
+
+    def parse_data(self, data):
+        self.nodes[data] = [True, 0]
+
+    def increment_ttl(self):
+        for node_key, node_value in self.nodes.items():
+            node_value[1] = node_value[1]+1
+            if node_value[1] > self.ttl:
+                node_value[0] = False
+
+    def add_node(self,name):
+        if name in self.nodes:
+            print("The selected node already exists");
+            return False
+        else:
+            self.nodes[name] = [True, self.ttl+1]
+            return True
+
+    def remove_node(self,name):
+        if name in self.nodes:
+            del self.nodes[name]
+            return True
+        else:
+            print("The selected node does not exist");
+            return False
--- Main.py
+++ Main.py
@@ -3,9 +3,18 @@ import threading
 import time
 
 
-x = HeartBeat('127.0.0.1', '127.0.0.1', 50008)
-x.startReceiving()
+x = HeartBeat(
+    name = "Me",
+    ip='127.0.0.1',
+    broadcast='127.0.0.1',
+    port=50001,
+    heartbeat_interval=2,
+    ttl=10
+)
+x.start_receiving()
 print("hi")
-x.send()
+x.start_sending()
+input('Press enter to manually add node named TestNode:')
+x.add_node("TestNode")
 input('Press enter to stop server:')
 x.stop()
